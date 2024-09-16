9dcbfb62b3b689aa1a38be6ec67eaee7d8128ec4
suhailabdaz <suhail.abdaz@gmail.com>
2024-09-16 19:27:10 +0530

    allowe_orgin_ci_cd_v2

--- src/utils/rateLimitter.ts
+++ src/utils/rateLimitter.ts
@@ -3,6 +3,6 @@ import { rateLimit } from 'express-rate-limit'
 export const limiter = rateLimit({
 	windowMs: 15 * 60 * 1000,
 	limit: 1000,
-	standardHeaders: 'draft-7',
+	standardHeaders: true,
 	legacyHeaders: false,
 })
